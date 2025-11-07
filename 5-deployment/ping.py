52fff3d64e24d0f90c12a6e98d8a504cfffa609c
zeinasamer <162062477+zeinasamer@users.noreply.github.com>
2025-11-07 21:29:58 +0000

    module 5

---
+++ 5-deployment/ping.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(title="ping")
+
+@app.get("/ping")
+def ping():
+    return "PONG"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
