e53ab4032c14b308fc8425ad31171590cb7db412
zeinasamer <162062477+zeinasamer@users.noreply.github.com>
2025-11-10 22:10:32 +0000

    module 5

--- 5-deployment/predict.py
+++ 5-deployment/predict.py
@@ -4,7 +4,45 @@ from typing import Dict, Any
 
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, conint, confloat
+from typing import Literal
+
+
+#request
+
+class Customer(BaseModel):
+    gender: Literal["male", "female"]
+    seniorcitizen: Literal[0, 1]
+    partner: Literal["yes", "no"]
+    dependents: Literal["yes", "no"]
+    phoneservice: Literal["yes", "no"]
+    multiplelines: Literal["no", "yes", "no_phone_service"]
+    internetservice: Literal["dsl", "fiber_optic", "no"]
+    onlinesecurity: Literal["no", "yes", "no_internet_service"]
+    onlinebackup: Literal["no", "yes", "no_internet_service"]
+    deviceprotection: Literal["no", "yes", "no_internet_service"]
+    techsupport: Literal["no", "yes", "no_internet_service"]
+    streamingtv: Literal["no", "yes", "no_internet_service"]
+    streamingmovies: Literal["no", "yes", "no_internet_service"]
+    contract: Literal["month-to-month", "one_year", "two_year"]
+    paperlessbilling: Literal["yes", "no"]
+    paymentmethod: Literal[
+        "electronic_check",
+        "mailed_check",
+        "bank_transfer_(automatic)",
+        "credit_card_(automatic)",
+    ]
+    tenure: conint(ge=0)  # based on min=0, max=72
+    monthlycharges: confloat(ge=0)
+    totalcharges: confloat(ge=0)
+
+#response
+
+class PredictResponse(BaseModel):
+    churn_probability: float
+    churn: bool
+
+    
 
 app = FastAPI(title="churn-prediction")
 
@@ -17,13 +55,13 @@ def predict_single(costumer):
     return float(result)
 
 @app.post("/predict")
-def predict(customer: Dict[str, Any]):
-    prob = predict_single(customer)
+def predict(customer: Customer) -> PredictResponse:
+    prob = predict_single(customer.model_dump())
 
-    return{
-        "churn_probability" : prob,
-        "churn" : bool(prob >= 0.5)
-    }
+    return PredictResponse(
+        churn_probability=prob,
+        churn=bool(prob >= 0.5)
+    )
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9696)
